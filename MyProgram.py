8c70c112e7d4cbe6e120f5c2bdf9dd4ac9e46a8a
N-Ibrahim <52959966+N-Ibrahim@users.noreply.github.com>
2023-10-31 15:17:49 +0000

    add new files
    new files added

---
+++ MyProgram.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 21:38:44 2023
+
+@author: nasir.ibrahim
+"""
+
+def add(first_number, second_number):
+    return first_number + second_number
+
+def subtract(first_number, second_number):
+    return first_number - second_number
+
+def multiply(first_number, second_number):
+    return first_number * second_number
+
+def divide(first_number, second_number):
+    return first_number / second_number
+    
+
+print(add(-5,7))
+print(subtract(-5,7))
+print(multiply(-5,7))
